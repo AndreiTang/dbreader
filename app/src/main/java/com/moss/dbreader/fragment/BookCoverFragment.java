92282f78bc66bba8e7ffbe9c4cfa3319e1bcd30b
Tang <qi-feng.tang@hp.com>
2017-11-14 07:41:46 +0800

    add book cover

---
+++ app/src/main/java/com/moss/dbreader/fragment/BookCoverFragment.java
@@ -0,0 +1,10 @@
+package com.moss.dbreader.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by tangqif on 11/13/2017.
+ */
+
+public class BookCoverFragment extends Fragment {
+}
